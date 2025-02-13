cfeba265b436fb706d462a85f74a0b12dfba4594
lee <41419347+plm-lee@users.noreply.github.com>
2025-02-13 16:49:25 +0800

    UI

--- lib/page/mine_page.dart
+++ lib/page/mine_page.dart
@@ -49,10 +49,18 @@ class MinePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset(
-              'assets/images/no_results.png',
+            Container(
               width: 100,
               height: 100,
+              decoration: BoxDecoration(
+                color: const Color(0xFF1E1E1E),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: const Icon(
+                Icons.folder_outlined,
+                size: 50,
+                color: Colors.white,
+              ),
             ),
             const SizedBox(height: 16),
             const Text(
